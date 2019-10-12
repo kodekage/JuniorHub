4d4b762b47c270c3c2ab959236d8a8b26cf0196c
Opara Prosper <oparaprosper79@gmail.com>
2019-10-12 15:51:22 +0100

    Remove unneccessary tag data

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class BlogsController extends Controller
 {
     public function index(){
-        return view('index')->with('tag', $tag = "Serving the developer community");
+        return view('index');
     }
 
     public function about(){
