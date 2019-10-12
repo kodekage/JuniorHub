26a60c0714754f23b9ac5716a271c046a1a5db20
Opara Prosper <oparaprosper79@gmail.com>
2019-10-12 15:52:20 +0100

    update view with project idea

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.layout')
 
 @section('content')
-  <div class="jumbotron text-center">
-    <h1>
-      Oparasblog is your GOTO for developer news
-    </h1>
+  <div class="jumbotron margin-down text-center">
+  <p>
+    {{config('app.name')}} helps you quicky put together your blog project, allowing you to focus on what's more important
+  </p>
   </div>
 @endsection
