637ab145fdb14b31242dbd44952fa69e487f1114
Opara Prosper <oparaprosper79@gmail.com>
2019-10-12 15:53:49 +0100

    Update project name

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
-<title>{{config('app.name', 'Oparasblog')}}</title>
+<title>{{config('app.name', 'laravel-blog-starter')}}</title>
 </head>
 <body>
   @include('includes.nav')
