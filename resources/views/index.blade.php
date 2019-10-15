9aebbb51b4aa0ba3cf303cb1267e2f2459af4eaf
Opara Prosper <oparaprosper79@gmail.com>
2019-10-15 21:01:01 +0100

    Add login/signup routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <div class="col">
       <h1 id="h1">{{config('app.name')}}</h1>
       <p>write your way to the senior rank</p>
-      <a href="#" class="btn btn-dark">sign up</a>
-      <a href="#" class="btn btn-outline-secondary">register</a>
+      <a href="/login" class="btn btn-dark">login</a>
+      <a href="/register" class="btn btn-outline-dark">register</a>
     </div>
     <div class="col-8 col-sm-8 col-md-6 col-lg-6 col-xl-6">
       <img class="img-responsive img-fluid rounded" src="{{asset('images/senior.jpg')}}" alt="">
