4326201e169adea794270594a89c5f3989e66976
Opara Prosper <oparaprosper79@gmail.com>
2019-10-21 20:44:37 +0100

    Update redirect to profile

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = '/profile';
 
     /**
      * Create a new controller instance.
