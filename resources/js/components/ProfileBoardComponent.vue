d863119a031b053862ef1b7f77cc03106396a659
Opara Prosper <oparaprosper79@gmail.com>
2019-10-25 19:55:54 +0100

    Add vue component for the profile view dashboard

---
+++ resources/js/components/ProfileBoardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-lg-8">
+        <div class="row align-items-center justify-content-between profile-tab">
+            <div class="col active">
+                Overiview
+            </div>
+            <div class="col">
+                Projects
+            </div>
+            <div class="col">
+                 Posts <mark class="round-fig">  {{ title }}</mark>
+            </div>
+            <div class="col">
+                Followers
+            </div>
+            <div class="col">
+                Profile views
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            required: true
+        }
+    }
+}
+</script>
