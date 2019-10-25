7b0f06ac7f0de58c3d03ca6aba9ee3ca92a7425e
Opara Prosper <oparaprosper79@gmail.com>
2019-10-25 19:55:00 +0100

    Test compnent on about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,4 +8,7 @@
     {{config('app.name')}} helps you quicky put together your blog project, allowing you to focus on what's more important
   </p>
   </div>
+  {{-- <div id="root"> --}}
+      {{-- <about></about> --}}
+  {{-- </div> --}}
 @endsection
