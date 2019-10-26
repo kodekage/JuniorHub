c2ccbdcfaac32adf89153e4dc443e7764d921f2b
Opara Prosper <oparaprosper79@gmail.com>
2019-10-26 13:18:12 +0100

    Add VueRouter, register router

--- resources/js/components/ProfileBoardComponent.vue
+++ resources/js/components/ProfileBoardComponent.vue
@@ -1,31 +1,70 @@
 <template>
     <div class="col-lg-8">
         <div class="row align-items-center justify-content-between profile-tab">
-            <div class="col active">
+            <router-link to='/overview' class="col active">
                 Overiview
-            </div>
-            <div class="col">
+            </router-link>
+            <router-link to="/projects" class="col">
                 Projects
-            </div>
-            <div class="col">
-                 Posts <mark class="round-fig">  {{ title }}</mark>
-            </div>
-            <div class="col">
+            </router-link>
+            <router-link :to="{ name: 'Posts', params: { user_post } }" class="col">
+                 Posts <mark class="round-fig">  {{ JSON.parse(post).length }} </mark>
+            </router-link>
+            <router-link to="/followers" class="col">
                 Followers
-            </div>
+            </router-link>
             <div class="col">
                 Profile views
             </div>
         </div>
+
+        <router-view class="active"></router-view>
     </div>
 </template>
 
 <script>
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/overview',
+            // name: 'overview',
+            component: require('./profile/OverviewComponent').default
+        },
+        {
+            path: '/projects',
+            component: require('./profile/ProjectsComponent').default
+        },
+        {
+            path: '/posts',
+            name: 'Posts',
+            component: require('./profile/PostsComponent').default,
+            props: true
+        },
+        {
+            path: '/followers',
+            component: require('./profile/FollowersComponent').default
+        }
+    ]
+});
+
 export default {
     props: {
-        title: {
+        post: {
             required: true
         }
-    }
+    },
+    data(){
+        return {
+            user_post: null
+        }
+    },
+    mounted(){
+        this.user_post = JSON.parse(this.post)
+    },
+  router
 }
 </script>
