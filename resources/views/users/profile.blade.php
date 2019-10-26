8ba51a76215471efac55915b2b6acfe4427edba3
Opara Prosper <oparaprosper79@gmail.com>
2019-10-26 13:19:59 +0100

    Add components to the profile details and board

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -5,7 +5,7 @@
     <div id="root" class="container-fluid margin-down">
         <div class="row justify-content-around">
             <profile-detail image="{{ asset("storage/profile_pictures/$user->profile_picture") }}" user="{{ $user }}"></profile-detail>
-            <profile-board title="{{ count($user->blogpost) }}"></profile-board>
+            <profile-board post="{{ $user->blogpost }}"></profile-board>
         </div>
     </div>
 
